5919a0db57d9c58dcbb333f153a742f682fe43dc
luiza <silveiraluizaa@gmail.com>
2021-08-24 20:40:40 -0300

    atualiza após rodar todos com gauss

--- utils.py
+++ utils.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 def remove_augmentation():
     
-    if len(os.list_dir('/home/dell/Documentos/rn-cin-covid/data/tmp/')) > 0:
-        raise Exception("Já há dados no diretório")
+    #if len(os.listdir('/home/dell/Documentos/rn-cin-covid/data/tmp/')) > 0:
+    #    raise Exception("Já há dados no diretório")
         
     train_labels_gauss = pd.read_csv("data/train_labels_gauss.txt", sep=" ",header=None)
     train_labels_gauss.columns = ['id', 'image', 'label', 'database']
@@ -20,6 +20,9 @@ def remove_augmentation():
     path = '/home/dell/Documentos/rn-cin-covid/'
     temp_dir_train = '/home/dell/Documentos/rn-cin-covid/data/tmp/train'
     temp_dir_val = '/home/dell/Documentos/rn-cin-covid/data/tmp/val'
+       
+    dir_train = '/home/dell/Documentos/rn-cin-covid/data/train'
+    dir_val = '/home/dell/Documentos/rn-cin-covid/data/val'
     
     os.chdir(path)
     
@@ -37,28 +40,28 @@ def remove_augmentation():
 
     # Copiando as imagens
     
-    for image in os.list_dir('data/train/positive'):
+    for image in os.listdir('data/train/positive'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(temp_dir_train, 'positive'))
+            shutil.move(os.path.join(dir_train,'positive',image), os.path.join(temp_dir_train, 'positive'))
             
-    for image in os.list_dir('data/train/negative'):
+    for image in os.listdir('data/train/negative'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(temp_dir_train, 'negative'))
+            shutil.move(os.path.join(dir_train,'negative',image), os.path.join(temp_dir_train, 'negative'))
             
-    for image in os.list_dir('data/val/positive'):
+    for image in os.listdir('data/val/positive'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(temp_dir_val, 'positive'))
+            shutil.move(os.path.join(dir_val,'positive',image), os.path.join(temp_dir_val, 'positive'))
     
-    for image in os.list_dir('data/val/negative'):
+    for image in os.listdir('data/val/negative'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(temp_dir_val, 'negative'))
+            shutil.move(os.path.join(dir_val,'negative',image), os.path.join(temp_dir_val, 'negative'))
             
          
             
             
 def add_back_augmentation():
     
-    if len(os.list_dir('/home/dell/Documentos/rn-cin-covid/data/tmp/')) < 1:
+    if len(os.listdir('/home/dell/Documentos/rn-cin-covid/data/tmp/')) < 1:
         raise Exception("Não há dados no diretório")
         
         
@@ -78,21 +81,21 @@ def add_back_augmentation():
 
     # Copiando as imagens
     
-    for image in os.list_dir('data/tmp/train/positive'):
+    for image in os.listdir('data/tmp/train/positive'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(dir_train, 'positive'))
+            shutil.move(os.path.join(temp_dir_train,'positive',image), os.path.join(dir_train, 'positive'))
             
-    for image in os.list_dir('data/tmp/train/negative'):
+    for image in os.listdir('data/tmp/train/negative'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(dir_train, 'negative'))
+            shutil.move(os.path.join(temp_dir_train,'negative',image), os.path.join(dir_train, 'negative'))
             
-    for image in os.list_dir('data/tmp/val/positive'):
+    for image in os.listdir('data/tmp/val/positive'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(tdir_val, 'positive'))
+            shutil.move(os.path.join(temp_dir_val,'positive',image), os.path.join(tdir_val, 'positive'))
     
-    for image in os.list_dir('data/tmp/val/negative'):
+    for image in os.listdir('data/tmp/val/negative'):
         if image in train_labels_gauss['image'].tolist():
-            shutil.copy(image, os.path.join(dir_val, 'negative'))
+            shutil.move(os.path.join(temp_dir_val,'negative',image), os.path.join(dir_val, 'negative'))
             
             
     # Deletando diretórios
@@ -101,8 +104,6 @@ def add_back_augmentation():
     shutil.rmtree(temp_dir_val)
     
 
-         
-            
-            
+
             
 
